24c46cce346418c55a5c85140259fb49f7f3588b
Fabián Cruz <fabiancruz3773@gmail.com>
2021-07-07 23:20:33 -0500

    catch the type of operation

--- calculator.js
+++ calculator.js
@@ -1,15 +1,47 @@
 let display = document.getElementById('display');
-display.innerHTML = display.getAttribute('data-value');
+let numDisplay ='';
 
 function show() {
-    display.innerHTML = display.getAttribute('data-value');
+    display.innerHTML = numDisplay;
 }
 
 function calculator(value)
 {
     if(typeof(value) == 'number')
     {
-        display.setAttribute('data-value',value)
+        if(numDisplay.length<=7)
+        {
+            display.setAttribute('data-value', value)
+            numDisplay += display.getAttribute('data-value')
+        } else
+        {
+            alert('muchos numeros')
+        }
         show()
+    } else{
+        switch(value)
+        {
+            case "del":
+                alert("del")
+                break;
+            case "-":
+                alert("menos")
+                break;
+            case "+":
+                alert("mas")
+                break;
+            case "x":
+                alert("multiplicacion")
+                break;
+            case "divide":
+                alert("divison")
+                break;
+            case "reset":
+                alert("reset")
+                break;
+            case "equal":
+                alert("equal")
+                break;
+        }
     }
 }
